689ef76ffcd643cef194c2b31c79868baf5d97e8
romanunkurov <augustroman6@gmail.com>
2021-10-22 19:12:54 +0300

    initializing project

---
+++ src/main/java/ru/august/history/employment_history/EmploymentHistoryApplication.java
@@ -0,0 +1,13 @@
+package ru.august.history.employment_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentHistoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/august/history/employment_history/EmploymentHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.august.history.employment_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
