ab94bd2da262a979d4c6e9b7ad3ae1ab35a08dd5
romanunkurov <augustroman6@gmail.com>
2021-10-25 20:54:46 +0300

    done with restController queries

--- src/main/java/ru/august/history/employment_history/EmploymentHistoryController.java
+++ src/main/java/ru/august/history/employment_history/EmploymentHistoryController.java
@@ -1,9 +1,61 @@
 package ru.august.history.employment_history;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api/person")
 public class EmploymentHistoryController {
+
+    @Autowired
+    private EmploymentHistoryRepository repository;
+
+    @GetMapping("/{id}")
+    public List<Person> getAllEmployees() {
+        return repository.findAll();
+    }
+
+
+    //get employee by id with his/her job history
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        Person employee = repository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+        return ResponseEntity.ok().body(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Person> updateEmployee(@PathVariable(value = "id") Long employeeId,
+                                                    @RequestBody Person employeeDetails) throws ResourceNotFoundException {
+        Person employee = repository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+        employee.setEmail(employeeDetails.getEmail());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setFirstName(employeeDetails.getFirstName());
+        final Person updatedEmployee = repository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        Person employee = repository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+        repository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+
 
 }
---
+++ src/main/java/ru/august/history/employment_history/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package ru.august.history.employment_history;
+
+public class ResourceNotFoundException extends Exception {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
