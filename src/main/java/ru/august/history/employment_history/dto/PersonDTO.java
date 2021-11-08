2a507fbd0f47f2b0c4ed8cab0c2a161807ea1b28
Anatolii Kondratov <anatoliy.kondratov@open.ru>
2021-11-08 17:22:15 +0300

    clean code

--- src/main/java/ru/august/history/employment_history/dto/PersonDTO.java
+++ src/main/java/ru/august/history/employment_history/dto/PersonDTO.java
@@ -9,23 +9,16 @@ public class PersonDTO {
     private String firstName;
     @NotNull
     private String lastName;
-
     private String middleName;
-
     @Pattern(regexp = "([0-9]{2} [0-9]{2})")
     private String passportSeries;
-
     @Pattern(regexp = "([0-9]{3} [0-9]{3})")
     private String passportNumber;
-
     @Pattern(regexp = "([0-9]{3}-[0-9]{3}-[0-9]{3}-[0-9]{2})")
     private String snils;
-
     private String phoneNumber;
-
     @Pattern(regexp = "^[A-z0-9._%+-]+@[A-z0-9.-]+\\.[A-z]{2,6}$")
     private String email;
-
     @Pattern(regexp = "[0-9]{12}")
     private String inn;
 
